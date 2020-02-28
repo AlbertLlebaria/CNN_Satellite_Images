6ecd33ed4913c92826286f63005d9a0de251dfe6
Albert Llebaria Holda <Allebaria94@gmail.com>
2020-02-28 15:58:01 +0100

    - Starting the organization and creation of dataset - Added GIS libraries such as geopandas, rasterio, fiona - Added tensorflow libraries - Created simple model in main.py - Created script to create datasets - Create script to merge RGB bands with elevation data

---
+++ create_dataset.py
@@ -0,0 +1,163 @@
+import rasterio
+from rasterio.plot import show
+from rasterio.plot import show_hist
+from rasterio.mask import mask
+from shapely.geometry import box
+import geopandas as gpd
+from fiona.crs import from_epsg
+import pycrs
+import numpy as np
+import glob
+from keras.layers import Input, Conv2D
+from keras.models import Model
+from rasterio.windows import Window
+import os
+import utils 
+
+
+DATADIR = 'data/'
+
+RESHAPED = 16000
+
+
+TRAIN_WINDOW = Window(0, 0, 16000, 16000)
+VAL_WINDOW = Window(0, 16000, 16000, 16000)
+
+
+
+# for file in glob.glob(f'{DATADIR}geo/*.tif'):
+# with rasterio.open(f'{DATADIR}geo/CPH_k.tif') as src:
+#     train_data = src.read(window=TRAIN_WINDOW)
+#     val_data = src.read(window=VAL_WINDOW)
+#     data_profile = src.profile.copy()
+#     print(train_data.shape)
+#     #write training data    
+#     train_profile = data_profile.copy()
+#     train_profile.update({'height': TRAIN_WINDOW.height,
+#                         'width': TRAIN_WINDOW.width,
+#                         'transform': rasterio.windows.transform(TRAIN_WINDOW, data_profile['transform'])
+#                         })
+
+#     with rasterio.open(os.path.join(DATADIR, 'train_val/cph_k_train.tif'), 'w', **train_profile) as dst:
+#         dst.write(train_data)
+
+#     #write test data
+#     val_profile = data_profile.copy()
+#     val_profile.update({'height': VAL_WINDOW.height,
+#                         'width': VAL_WINDOW.width,
+#                         'transform': rasterio.windows.transform(VAL_WINDOW, data_profile['transform'])
+#                     }
+#                     )
+#     with rasterio.open(os.path.join(DATADIR, 'train_val/cph_k_val.tif'), 'w', **val_profile) as dst:
+#         dst.write(val_data)
+
+# with rasterio.open(f'{DATADIR}geo/mask_k.tif') as src:
+#     train_mask = src.read(window=TRAIN_WINDOW)
+#     val_mask = src.read(window=VAL_WINDOW)
+#     train_mask = np.where(train_mask > 0, 255, train_mask) 
+#     val_mask = np.where(val_mask > 0, 255, val_mask) 
+
+#     data_profile = src.profile.copy()
+
+#     #write training data
+#     train_profile = data_profile.copy()
+#     train_profile.update({'height': TRAIN_WINDOW.height,
+#                         'width': TRAIN_WINDOW.width,
+#                         'transform': rasterio.windows.transform(TRAIN_WINDOW, data_profile['transform'])
+#                         }
+#                         )
+
+#     with rasterio.open(os.path.join(DATADIR, 'train_val/mask_k_train.tif'), 'w', **train_profile) as dst:
+#         dst.write(train_mask)
+
+#     #write test data
+#     val_profile = data_profile.copy()
+#     val_profile.update({'height': VAL_WINDOW.height,
+#                         'width': VAL_WINDOW.width,
+#                         'transform': rasterio.windows.transform(VAL_WINDOW, data_profile['transform'])
+#                     }
+#                     )
+#     with rasterio.open(os.path.join(DATADIR, 'train_val/mask_k_val.tif'), 'w', **val_profile) as dst:
+#         dst.write(val_mask)
+
+print('created train val')
+in_path = os.path.join(DATADIR, 'train_val')
+out_path = os.path.join(DATADIR, 'train_patches')
+input_filenames = [('cph_k_train.merged.tif', 'mask_k_train.tif')]
+
+output_filename = 'tile_{}-{}.tif'
+output_lab_filename = 'tile_labels_{}-{}.tif'
+
+for filename in input_filenames:
+    (input_filename, label_filename) = filename
+    with rasterio.open(os.path.join(in_path, input_filename)) as inds:
+        tile_width, tile_height = 224, 224
+        meta = inds.meta.copy()
+        nodata = meta['nodata']
+    
+        with rasterio.open(os.path.join(in_path, label_filename)) as labs:
+            meta_labels = labs.meta.copy()
+            labs_data = labs.read()
+            labs_num = len(np.unique(labs_data))
+            name_counter = 0
+            for window, transform in utils.get_tiles(inds, tile_width, tile_height):
+                
+                meta['transform'] = transform
+                meta['width'], meta['height'] = window.width, window.height
+                meta_labels['transform'] = transform
+                meta_labels['width'], meta_labels['height'] = window.width, window.height
+                data = inds.read(window=window)
+                labels = labs.read(window=window)
+                #if labs_num == 5:
+                #    print("found 5 labs in :  " + label_filename)
+                #    labels[labels == 5] = 6
+                if window.width ==  tile_width and window.height == tile_height: #and not np.all(data[5, :, :] == nodata):
+                    outpath = os.path.join(out_path, output_filename.format(input_filename[:2], name_counter))
+                    outpath_lab = os.path.join(out_path, output_lab_filename.format(input_filename[:2], name_counter))
+                    with rasterio.open(outpath, 'w', **meta) as outds:
+                        outds.write(data)
+                    with rasterio.open(outpath_lab, 'w', **meta_labels) as outls:
+                        outls.write(labels)
+                    name_counter += 1
+
+### Follow the same procedure to create patches for validation ###
+
+in_path = os.path.join(DATADIR, 'train_val')
+out_path = os.path.join(DATADIR, 'val_patches')
+input_filenames = [('cph_k_val.merged.tif', 'mask_k_val.tif')]
+
+output_filename = 'tile_{}-{}.tif'
+output_lab_filename = 'tile_labels_{}-{}.tif'
+
+for filename in input_filenames:
+    (input_filename, label_filename) = filename
+    with rasterio.open(os.path.join(in_path, input_filename)) as inds:
+        tile_width, tile_height = 224, 224
+        meta = inds.meta.copy()
+        nodata = meta['nodata']
+    
+        with rasterio.open(os.path.join(in_path, label_filename)) as labs:
+            meta_labels = labs.meta.copy()
+            labs_data = labs.read()
+            labs_num = len(np.unique(labs_data))
+            print(labs_num)
+            name_counter = 0
+            for window, transform in utils.get_tiles(inds, tile_width, tile_height):
+                
+                meta['transform'] = transform
+                meta['width'], meta['height'] = window.width, window.height
+                meta_labels['transform'] = transform
+                meta_labels['width'], meta_labels['height'] = window.width, window.height
+                data = inds.read(window=window)
+                labels = labs.read(window=window)
+                #if labs_num == 5:
+                #    print("found 5 labs in :  " + label_filename)
+                #    labels[labels == 5] = 6
+                if window.width ==  tile_width and window.height == tile_height: #and not np.all(data[5, :, :] == nodata):
+                    outpath = os.path.join(out_path, output_filename.format(input_filename[:2], name_counter))
+                    outpath_lab = os.path.join(out_path, output_lab_filename.format(input_filename[:2], name_counter))
+                    with rasterio.open(outpath, 'w', **meta) as outds:
+                        outds.write(data)
+                    with rasterio.open(outpath_lab, 'w', **meta_labels) as outls:
+                        outls.write(labels)
+                    name_counter += 1
---
+++ main.py
@@ -0,0 +1,176 @@
+# reading training data and labels
+from segmentation_models.utils import set_trainable
+from segmentation_models.metrics import iou_score
+from segmentation_models.losses import jaccard_loss
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping, TensorBoard
+import glob
+import numpy as np
+import rasterio
+import os
+import segmentation_models as sm
+from segmentation_models import Unet
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+import keras
+import tensorflow as tf
+from matplotlib.pyplot import imshow
+from keras.utils import Sequence
+from albumentations import (Compose, RandomCrop, HorizontalFlip, VerticalFlip, RandomRotate90, Transpose,
+                            ShiftScaleRotate, RandomGamma, IAAEmboss, Blur, OneOf,
+                            ElasticTransform, GridDistortion, OpticalDistortion, ToFloat)
+
+from keras.layers import Input, Conv2D
+from keras.models import Model
+import utils
+from matplotlib import pyplot as plt
+import time
+
+
+
+DATADIR = 'data/'
+
+
+patch_path = os.path.join(DATADIR, 'train_patches/')
+
+X_train = np.empty((400, 224, 224, 5), dtype=np.float32)
+y_train = np.empty((400, 224, 224), dtype=np.int8)
+
+for index, filename in enumerate(sorted(glob.glob(f'{patch_path}tile_cp*.tif'))):
+    if(index >= 400):
+        break
+    print(filename)
+    print(index)
+    with rasterio.open(filename, 'r') as src:
+        data = src.read([1, 2, 3, 4, 6])
+        print(src.nodatavals)
+        profile = src.profile
+    nodata = profile['nodata']
+    data = np.swapaxes(data, 0, 1)
+    data = np.swapaxes(data, 1, 2)
+    X_train[index, :, :, :] = data
+
+for index, filename in enumerate(sorted(glob.glob(f'{patch_path}tile_labels_cp*.tif'))):
+    if(index >= 400):
+        break
+    print(filename)
+    print(index)
+    with rasterio.open(filename, 'r') as src:
+        labels = src.read()
+        labels = labels.astype(np.int8)
+    np.where(labels == 255, 1, labels)
+    y_train[index, :, :] = labels[0, :, :]
+
+# reading validation data and labels
+patch_path = os.path.join(DATADIR, 'val_patches/')
+
+X_val = np.empty((200, 224, 224, 5), dtype=np.float32)
+y_val = np.empty((200, 224, 224), dtype=np.int8)
+
+for index, filename in enumerate(sorted(glob.glob(f'{patch_path}tile_cp*.tif'))):
+    if(index >= 200):
+        break
+    with rasterio.open(filename, 'r') as src:
+        data = src.read([1, 2, 3, 4, 6])
+        profile = src.profile
+    nodata = profile['nodata']
+    data = np.swapaxes(data, 0, 1)
+    data = np.swapaxes(data, 1, 2)
+    X_val[index, :, :, :] = data
+
+for index, filename in enumerate(sorted(glob.glob(f'{patch_path}tile_labels_cp*.tif'))):
+    if(index >= 200):
+        break
+    print(filename)
+    print(index)
+    with rasterio.open(filename, 'r') as src:
+        labels = src.read()
+        labels = labels.astype(np.int8)
+    np.where(labels == 255, 1, labels)
+    y_val[index, :, :] = labels[0, :, :]
+
+
+for band in range(X_train.shape[3]):
+    minX = np.min(X_train[:, :, :, band])
+    maxX = np.max(X_train[:, :, :, band])
+    X_train[:, :, :, band] = (X_train[:, :, :, band] - minX) / (maxX - minX)
+    X_val[:, :, :, band] = (X_val[:, :, :, band] - minX) / (maxX - minX)
+
+
+# sequence to load data and apply augmentation
+y_val = y_val.reshape((200, 224, 224, 1))
+y_train = y_train.reshape((400, 224, 224, 1))
+
+
+class UnetSequence(Sequence):
+    def __init__(self, x_set, y_set, batch_size, augmentations):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+        self.augment = augmentations
+
+    def __len__(self):
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        data_index_min = int(idx*self.batch_size)
+        data_index_max = int(min((idx+1)*self.batch_size, len(self.x)))
+        
+        indexes = self.x[data_index_min:data_index_max]
+        this_batch_size = len(indexes)
+        
+        X = np.empty((this_batch_size, 224, 224, 5), dtype=np.float32)
+        y = np.empty((this_batch_size, 224, 224, 1), dtype=np.uint8)
+        
+        for i, sample_index in enumerate(indexes):
+            x_sample = self.x[idx, :, :, :]
+            y_sample =self.y[idx,:,:]
+            if self.augment is not None:
+                augmented = self.augment(image=x_sample, mask=y_sample)
+                image_augm = augmented['image']
+                mask_augm = augmented['mask']
+                X[i, :, :, :] = image_augm
+                y[i, :, :, :] = mask_augm
+            else:
+                X[i, :, :, :] = x_sample
+                y[i, :, :, :] = y_sample
+        return X, y
+
+train_augm = Compose([
+    HorizontalFlip(p=0.5),
+    VerticalFlip(p=0.5),
+    RandomRotate90(p=0.5),
+    Transpose(p=0.5),
+], p=1)
+
+base_model = Unet(backbone_name='efficientnetb0',
+                  encoder_weights='imagenet', encoder_freeze=True)
+inp = Input(shape=(None, None, 5))
+l1 = Conv2D(3, (1, 1))(inp)  # map N channels data to 3 channels
+out = base_model(l1)
+model = Model(inp, out, name=base_model.name)
+
+
+# reduces learning rate on plateau
+lr_reducer = ReduceLROnPlateau(factor=0.1,
+                               cooldown=10,
+                               patience=10, verbose=1,
+                               min_lr=0.1e-5)
+# model autosave callbacks
+mode_autosave = ModelCheckpoint("./weights/unet.h5",
+                                monitor='val_iou_score',
+                                mode='max', save_best_only=True, verbose=1, period=1)
+
+
+callbacks = [lr_reducer, mode_autosave]
+
+
+lre = 0.001
+adam = Adam(lr=lre)
+model.compile(adam, jaccard_loss, ['binary_accuracy', iou_score])
+
+train_gen = UnetSequence(X_train, y_train, 8, augmentations=None)
+test_gen = UnetSequence(X_val, y_val, 8, augmentations=None)
+
+
+print(model.summary())
+model.load_weights("./weights/unet.h5")
